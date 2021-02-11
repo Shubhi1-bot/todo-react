ee93865d018446dc33d2eb87f82895cb64208e33
Shubhi Mishra <shubhimishra053@gmail.com>
2021-02-11 17:13:16 +0530

    my first react app

---
+++ src/component/Form.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+
+const Form = () =>
+{
+return(
+    <form>
+        <input type="text" class="todo-input"/>
+        <button class="todo-button" type="submit">
+        
+        </button>
+        <div class="select">
+            <select name="todos" class="filter-todo">
+                <option value="all">All</option>
+                <option value="completed">Completed</option>
+                <option value="uncompleted">uncompleted</option>
+            </select>
+        </div>
+    </form>
+);
+}
+export default Form;
